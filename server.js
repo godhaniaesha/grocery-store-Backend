41408f87a2a4254553192ee9a9b16978175ad674
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2025-05-01 09:19:04 +0530

    asd

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const express = require("express");
 const { connectDB } = require("./db/db");
 const indexRoutes = require("./routes/indexRoutes");
 const server = express();
-const port = process.env.PORT;
-const path = require('path')
+const port = process.env.PORT || 4000;
+const path = require('path');
 const cors = require('cors');
 
 server.use(express.json());
 
 server.use(cors({
   origin: 'http://localhost:3000',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Increase the timeout for the server
+server.timeout = 60000; // 60 seconds
+
 server.use("/api", indexRoutes);
-server.use('/public', express.static(path.join(__dirname, 'public')))
+server.use('/public', express.static(path.join(__dirname, 'public')));
 
 server.listen(port, () => {
   connectDB();
